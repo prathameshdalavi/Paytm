6f3d40fcd86e021c829cd99b0a09161b22f51d21
Prathamesh Dalavi <prathameshdalavi04@gmail.com>
2025-02-14 23:18:54 +0530

    updated signup page

--- paytm-frontend/src/pages/signup.tsx
+++ paytm-frontend/src/pages/signup.tsx
@@ -3,28 +3,28 @@ import { BackendUrl } from "../config"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 export function Signup() {
-    const navigate=useNavigate()
-    const emailRef=useRef<HTMLInputElement>(null)
-    const passwordRef=useRef<HTMLInputElement>(null)
-    const firstNameRef=useRef<HTMLInputElement>(null)
-    const lastNameRef=useRef<HTMLInputElement>(null)
-    async function signUp(){
-        const email=emailRef.current?.value
-        const password=passwordRef.current?.value
-        const firstName=firstNameRef.current?.value
-        const lastName=lastNameRef.current?.value
-        try{
-            await axios.post(BackendUrl+"/api/v1/signUp",{
-                email:email,
-                password:password,
-                firstName:firstName,
-                lastName:lastName
+    const navigate = useNavigate()
+    const emailRef = useRef<HTMLInputElement>(null)
+    const passwordRef = useRef<HTMLInputElement>(null)
+    const firstNameRef = useRef<HTMLInputElement>(null)
+    const lastNameRef = useRef<HTMLInputElement>(null)
+    async function signUp() {
+        const email = emailRef.current?.value
+        const password = passwordRef.current?.value
+        const firstName = firstNameRef.current?.value
+        const lastName = lastNameRef.current?.value
+        try {
+            await axios.post(BackendUrl + "/api/v1/signUp", {
+                email: email,
+                password: password,
+                firstName: firstName,
+                lastName: lastName
             })
 
             navigate("/signin");
             alert("Signed Up Successfully")
         }
-        catch(e){
+        catch (e) {
             console.log(e)
         }
     }
@@ -37,7 +37,21 @@ export function Signup() {
                 Enter Your creditials to access your account
             </div>
             <div className="flex flex-col gap-3 text-md">
-                <div className="font-semibold pt-4">
+                <div className="flex gap-2 pt-4 ">
+                    <div>
+                        <span className="font-semibold">
+                            First Name
+                        </span>
+                        <input type="text" ref={firstNameRef} className="border-1 border-gray-400 rounded-md items-center p-1" placeholder="Enter your first name" />
+                    </div>
+                    <div>
+                        <span className="font-semibold">
+                            Last Name
+                        </span>
+                        <input type="text" ref={lastNameRef} className="border-1 border-gray-400 rounded-md items-center p-1" placeholder="Enter your last name" />
+                    </div>
+                </div>
+                <div className="font-semibold">
                     Email
                 </div>
                 <input ref={emailRef} className="border-1 border-gray-400 rounded-md items-center p-1" type="text" placeholder="Enter your email" />
@@ -45,22 +59,9 @@ export function Signup() {
                     Password
                 </div>
                 <input ref={passwordRef} className="border-1 border-gray-400 rounded-md items-center p-1" type="text" placeholder="Enter your password" />
-                <div className="flex gap-2">
-                    <div>
-                        <span className="font-semibold">
-                        First Name
-                        </span>
-                        <input type="text" ref={firstNameRef} className="border-1 border-gray-400 rounded-md items-center p-1" placeholder="Enter your first name" />
-                    </div>
-                    <div>
-                        <span className="font-semibold">
-                        Last Name
-                        </span>
-                        <input type="text" ref={lastNameRef} className="border-1 border-gray-400 rounded-md items-center p-1" placeholder="Enter your last name" />
-                    </div>
-                </div>
+
                 <div className="mt-6">
-                <button className=" text-white w-full p-2 cursor-pointer rounded-md bg-black flex items-center justify-center" onClick={signUp} >Sign Up</button>
+                    <button className=" text-white w-full p-2 cursor-pointer rounded-md bg-black flex items-center justify-center" onClick={signUp} >Sign Up</button>
                 </div>
             </div>
             <div className="pt-4 flex justify-center">
